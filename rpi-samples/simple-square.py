ea6bd38ac6d0df14bee97d5a0caa0d0fdcfcbf4e
Matthew Gerling <matthewsgerling@gmail.com>
2018-03-10 12:24:37 -0600

    Simple Square

--- rpi-samples/simple-square.py
+++ rpi-samples/simple-square.py
@@ -14,11 +14,11 @@ class SimpleSquare(SampleBase):
                 offset_canvas.SetPixel(offset_canvas.height - 1 - x, x, 255, 0, 255)
 
             for x in range(0, offset_canvas.width):
-                offset_canvas.SetPixel(x, 0, 255, 0, 0)
+                offset_canvas.SetPixel(0, x, 255, 0, 0)
                 offset_canvas.SetPixel(x, offset_canvas.height - 1, 255, 255, 0)
 
             for y in range(0, offset_canvas.height):
-                offset_canvas.SetPixel(0, y, 0, 0, 255)
+                offset_canvas.SetPixel(y, 0, 0, 0, 255)
                 offset_canvas.SetPixel(offset_canvas.width - 1, y, 0, 255, 0)
             offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
 
